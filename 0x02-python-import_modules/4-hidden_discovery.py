fd3688799f8d79e9dee28b8783904b7733e9863c
powercodes <juliewanja6@gmail.com>
2024-04-12 14:40:54 -0700

    program that prints all the names defined by the compiled module

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -3,6 +3,6 @@ if __name__ == "__main__":
     from hidden_4 import *
     allf = dir()
     for i in range(0, len(allf)):
-        if allf[i][:2] != "_":
+        if allf[i][:2] != "__":
             print("{:s}".format(allf[i]))
 
